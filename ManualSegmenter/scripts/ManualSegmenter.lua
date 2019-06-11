31ef0cc590cfa239397df2d6cdc27492c39baf96
Florian Roser <Florian.Roser@sick.de>
2019-06-11 17:15:15 +0200

    Initial commit

---
+++ ManualSegmenter/scripts/ManualSegmenter.lua
@@ -0,0 +1,86 @@
+--Start of Global Scope---------------------------------------------------------
+print('AppEngine Version: ' .. Engine.getVersion())
+
+-- Delay in ms between visualization steps for demonstration purpose only
+local DELAY = 100
+
+-- Creating viewer
+local viewer = View.create()
+
+-- Setting up graphical overlay attributes
+local decoration = View.ShapeDecoration.create()
+decoration:setLineColor(0, 255, 0)
+decoration:setLineWidth(3)
+local charDeco = View.TextDecoration.create()
+charDeco:setSize(60)
+charDeco:setColor(0, 255, 0)
+
+-- Creating and setting up an OCR segmenter
+local segmenter = Image.OCR.Halcon.ManualSegmenter.create()
+segmenter:setParameter('POLARITY', 'dark_on_light')
+segmenter:setParameter('CHAR_WIDTH', 58)
+segmenter:setParameter('CHAR_HEIGHT', 65)
+segmenter:setParameter('STROKE_WIDTH', 7)
+segmenter:setParameter('IS_DOTPRINT', 'true')
+segmenter:setParameter('RETURN_SEPARATORS', 'false')
+segmenter:setParameter('UPPERCASE_ONLY', 'true')
+segmenter:setParameter('ELIMINATE_HORIZONTAL_LINES', 'true')
+segmenter:setParameter('BASE_LINE_TOLERANCE', 0.25)
+segmenter:setParameter('TEXT_LINE_STRUCTURE_0', '10')
+segmenter:setParameter('MAX_LINE_NUM', 1)
+segmenter:setParameter('ADD_FRAGMENTS', true)
+segmenter:setParameter('FRAGMENT_SIZE_MIN', 20)
+
+-- Creating font classifier and set font
+local fontClassifier = Image.OCR.Halcon.FontClassifier.create()
+fontClassifier:setFont('UNIVERSAL_0_9A_Z_NOREJ')
+
+--End of Global Scope-----------------------------------------------------------
+
+--Start of Function and Event Scope---------------------------------------------
+
+local function main()
+  for i = 1, 6 do -- 6 images in test set
+    -- Load image
+    local im = Image.load('resources/gulasch_label_' .. i .. '.bmp')
+    viewer:clear()
+    viewer:addImage(im)
+    viewer:present()
+
+    -- Get the sub image where the text is
+    local cropIm = Image.crop(im, 200, 1100, 1200, 550)
+    -- Make the text dark on light background
+    Image.invertInplace(cropIm)
+
+    -- Find and segment text in rotated image
+    local textLines = segmenter:findText(cropIm)
+    local charRegions = textLines:getTextLine(0)
+    Script.sleep(DELAY * 7)
+
+    if charRegions ~= nil then
+      -- Classify all found text regions
+      local regExp = '(BF[0-9]{8})'
+      local characters,
+        _,
+        _ = fontClassifier:classifyCharacters(charRegions, cropIm, regExp)
+
+      -- Draw bounding boxes and print characters
+      for j = 1, #charRegions do
+        local box = charRegions[j]:getBoundingBox()
+        box = box:translate(200, 1100)
+        viewer:addShape(box, decoration)
+        local CoG = box:getCenterOfGravity()
+        charDeco:setPosition(CoG:getX() - 15, CoG:getY() - 80)
+        viewer:addText(characters:sub(j, j), charDeco)
+        viewer:present() -- can be put outside loop if not for demonstration
+        Script.sleep(DELAY) -- for demonstration purpose only
+      end
+    end
+  end
+
+  print('App finished.')
+end
+
+Script.register('Engine.OnStarted', main)
+
+--End of Function and Event Scope--------------------------------------------------
---
+++ readme.md
@@ -0,0 +1,21 @@
+## ManualSegmenter
+Reading the "Best before date" on a food package, using API integrated
+HALCON ManualSegmenter.
+### Description
+HALCON ManualSegmenter provides a precise way to read text with known
+characteristics. All known text characteristics can be entered as parameters.
+### How to Run
+Starting this sample is possible either by running the app (F5) or
+debugging (F7+F10). Setting breakpoint on the first row inside the 'main'
+function allows debugging step-by-step after 'Engine.OnStarted' event.
+Results can be seen in the image viewer on the DevicePage.
+Restarting the Sample may be necessary to show images after loading the webpage.
+To run this Sample a device with SICK Algorithm API, HALCON support and
+AppEngine >= V2.5.0 is required. For example SIM4000 with latest firmware.
+Alternatively the Emulator in AppStudio 2.3 or higher can be used in conjunction
+with a HALCON developer license and dongle.
+### More Information
+Tutorial "Algorithms - OCR"
+
+### Topics
+Algorithm, Image-2D, OCR, Sample, SICK-AppSpace
